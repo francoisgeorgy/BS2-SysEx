a62015f0d8a1d7465d5701accb6eab3043a5fc7c
François Georgy <francois.georgy@gmail.com>
2017-04-12 23:15:56 +0200

    rev eng data

--- BS2-SysEx-format.md
+++ BS2-SysEx-format.md
@@ -108,11 +108,27 @@ Example: TODO...
         pul.wi. min=0000000
                 max=1111111
     
+## Values accross bytes
 
+-127..127:
+
+Example with Osc1 LFO1 Depth 
+
+    mask:   00011111 01110000
+
+    -127:   xxx00000 x000xxxx   00000 000 = 0         0 - 127 = -127
+     -56:   xxx01000 x111xxxx   01000 111 = 71       71 - 127 = -56
+      -1:   xxx01111 x110xxxx   01111 110 = 126     126 - 127 = -1 
+       0:   xxx10000 x000xxxx   10000 000 = 128     128 - 128 = 0
+       1:   xxx10000 x001xxxx   10000 001 = 129     129 - 128 = 1 
+      56:   xxx10111 x000xxxx   10111 000 = 184     184 - 128 = 56
+     127:   xxx11111 x111xxxx   11111 111 = 255     255 - 128 = 127
+
+     rule: if msb==1 substract 128 else substract 127
 
 ## Two-bytes CC
 
-Value = 23
+Example with value = 23
 
     23 / 2 --> 11
     23 % 2 --> 1 --> 64
@@ -120,7 +136,7 @@ Value = 23
     MSB: 11 in binary = 00001011
     LSB: 64 in binary = 01000000
 
-    shift MSB one bit to left and add LSB:
+    shift MSB one bit to left and add LSB bit:
     
         00010110
       + 00000001
@@ -129,7 +145,7 @@ Value = 23
        
     binary 00010111 = decimal 23        
 
-Value = 231                                  
+Example with value = 231                                  
     
     231 / 2 --> 115
     231 % 2 --> 1 --> 64
@@ -137,7 +153,7 @@ Value = 231
     MSB: 115 in binary = 01110011
     LSB:  64 in binary = 01000000
 
-    shift MSB one bit to left and add LSB:
+    shift MSB one bit to left and add LSB bit:
     
         11100110
       + 00000001
@@ -145,6 +161,8 @@ Value = 231
         11100111
        
     binary 11100111 = decimal 231       
+
+----
 
 ## Master
 
--- src/bs2-sysex.js
+++ src/bs2-sysex.js
@@ -10,13 +10,133 @@ export const BS2_SYSEX = [
         shift: 0,
         range: [0, 0],
         description: "start of sysex data"
-    },{
-        offset: 1,
-        bytes: 0,
-        masks: [0b0],
+    }, {
+        offset: 20,
+        bytes: 2,
+        masks: [0x07, 0x78],
+        shift: 0,
+        range: [63, 66],
+        description: "Osc 1 Range"
+    },
+    {
+        offset: 21,
+        bytes: 2,
+        masks: [0x03, 0x7C],
+        shift: 0,
+        range: [-12.0, 12.0],
+        description: "Osc 1 coarse"
+    }, {
+        offset: 22,
+        bytes: 2,
+        masks: [0x03, 0x7E],
+        shift: 0,
+        range: [-100, 100],
+        description: "Osc 1 fine"
+    }, {
+        offset: 26,
+        bytes: 2,
+        masks: [0x1F, 0x20],
+        shift: 0,
+        range: [63, 66],
+        description: "Osc 2 Range"
+    }, {
+        offset: 27,
+        bytes: 2,
+        masks: [0x1F, 0x70],
+        shift: 0,
+        range: [-12.0, 12.0],
+        description: "Osc 2 coarse"
+    },  {
+        offset: 28,
+        bytes: 2,
+        masks: [0x0F, 0x78],
+        shift: 0,
+        range: [-100, 100],
+        description: "Osc 2 fine"
+    }, {
+        offset: 98,
+        bytes: 2,
+        masks: [0x1F, 0x60],
+        shift: 0,
+        range: [-63, 63],
+        description: "Osc 1 Mod Env depth"
+    }, {
+        offset: 37,
+        bytes: 1,
+        masks: [0x08],
         shift: 0,
         range: [0, 0],
-        description: ""
-    }
+        description: "Sub Osc octave"
+    }, {
+        offset: 44,
+        bytes: 2,
+        masks: [0x0F, 0x78],
+        shift: 0,
+        range: [0, 255],
+        description: "Filter Frequency"
+    }, {
+        offset: 90,
+        bytes: 2,
+        masks: [0x3F, 0x60],
+        shift: 0,
+        range: [-127, 127],
+        description: "Osc 1 LFO 1 depth"
+    }, {
+        offset: 91,
+        bytes: 2,
+        masks: [0x1F, 0x70],
+        shift: 0,
+        range: [-127, 127],
+        description: "Osc 2 LFO 1 depth"
+    }, {
+        offset: 99,
+        bytes: 2,
+        masks: [0x0F, 0x70],
+        shift: 0,
+        range: [-63, 63],
+        description: "Osc 2 Mod Env depth"
+    }, {
+    offset: 101,
+    bytes: 2,
+    masks: [0x03,0x7C],
+    shift: 0,
+    range: [-90, 90],
+    description: "Osc 1 Pulse Width"
+},{
+    offset: 102,
+    bytes: 2,
+    masks: [0x01,0x7E],
+    shift: 0,
+    range: [-90, 90],
+    description: "Osc 2 Pulse Width"
+},{
+    offset: 66,
+    bytes: 2,
+    masks: [0x3F,0x60],
+    shift: 0,
+    range: [0, 255],
+    description: "LFO 1 Speed"
+},{
+    offset: 73,
+    bytes: 2,
+    masks: [0x7F,0x40],
+    shift: 0,
+    range: [0, 255],
+    description: "LFO 2 Speed"
+},{
+    offset: 64,
+    bytes: 1,
+    masks: [0x7F],
+    shift: 0,
+    range: [0, 127],
+    description: "LFO 1 Delay"
+},{
+    offset: 70,
+    bytes: 2,
+    masks: [0x01,0x7E],
+    shift: 0,
+    range: [0, 127],
+    description: "LFO 2 Delay"
+}
 ]
 
