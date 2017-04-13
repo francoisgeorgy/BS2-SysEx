1c9da673871703183d95539ada9537c8a9f0efe5
François Georgy <francois.georgy@gmail.com>
2017-04-13 23:00:30 +0200

    defining constants for the bs2 parameters

---
+++ src/bs2-const.js
@@ -0,0 +1,124 @@
+function buildSet(array) {
+    var set = {};
+    for (var i in array) {
+        var item = array[i];
+        set[item] = item;
+    }
+    return Object.freeze(set);
+}
+
+var BS2_PARAM = buildSet([
+// Master
+    'PATCH_VOLUME',
+// Oscillator 1
+    'OSC1_FINE',
+    'OSC1_RANGE',
+    'OSC1_COARSE',
+    'OSC1_WAVEFORM',
+    'OSC1_MOD_ENV_DEPTH',
+    'OSC1_LFO1_DEPTH',
+    'OSC1_MOD_ENV_PW_MOD',
+    'OSC1_LFO2_PW_MOD',
+    'OSC1_MANUAL_PW',
+// Oscillator 2
+    'OSC2_FINE',
+    'OSC2_RANGE',
+    'OSC2_COARSE',
+    'OSC2_WAVEFORM',
+    'OSC2_MOD_ENV_DEPTH',
+    'OSC2_LFO1_DEPTH',
+    'OSC2_ENV2_PW_MOD',
+    'OSC2_LFO2_PW_MOD',
+    'OSC2_MANUAL_PW',
+    'SUB_OSC_OCT',
+    'SUB_OSC_WAVE',
+// Mixer
+    'MIXER_OSC_1_LEVEL',
+    'MIXER_OSC_2_LEVEL',
+    'MIXER_SUB_OSC_LEVEL',
+    'MIXER_SELECT_NOISE_RING_EXT',
+    'MIXER_NOISE_LEVEL',
+    'MIXER_RING_MOD_LEVEL',
+    'MIXER_EXTERNAL_SIGNAL_LEVEL',
+// Filter
+    'FILTER_TYPE',
+    'FILTER_SLOPE',
+    'FILTER_SHAPE',
+    'FILTER_FREQUENCY',
+    'FILTER_RESONANCE',
+    'FILTER_MOD_ENV_DEPTH',
+    'FILTER_LFO2_DEPTH',
+    'FILTER_OVERDRIVE',
+// Portamento
+    'PORTAMENTO_TIME',
+// LFOs
+    'LFO1_SPEED',
+    'LFO1_DELAY',
+    'LFO2_SPEED',
+    'LFO2_DELAY',
+    'LFO1_WAVE',
+    'LFO2_WAVE',
+    'LFO1_SYNC_VALUE',
+    'LFO2_SYNC_VALUE',
+// Envelopes
+    'AMP_ENV_ATTACK',
+    'AMP_ENV_DECAY',
+    'AMP_ENV_SUSTAIN',
+    'AMP_ENV_RELEASE',
+    'AMP_ENV_TRIGGERING',
+    'MOD_ENV_ATTACK',
+    'MOD_ENV_DECAY',
+    'MOD_ENV_SUSTAIN',
+    'MOD_ENV_RELEASE',
+    'MOD_ENV_TRIGGERING',
+// Effects
+    'FX_DISTORTION',
+    'FX_OSC_FILTER_MOD',
+// Arpeggiator
+    'ARP_ON',
+    'ARP_LATCH',
+    'ARP_RHYTHM',
+    'ARP_NOTE_MODE',
+    'ARP_OCTAVES',
+// Other
+    'PITCH',
+    'MOD',
+    'SUSTAIN',
+    'AFTER_TOUCH',
+// Mod wheel
+    'NOD_WHEEL_LFO2_FILTER_FREQ',
+    'NOD_WHEEL_LFO1_OSC_PITCH',
+    'NOD_WHEEL_OSC2_PITCH',
+// Aftertouch
+    'AFTERTOUCH_FILTER_FREQ',
+    'AFTERTOUCH_LFO1_TO_OSC_PITCH',
+    'AFTERTOUCH_LFO2_SPEED',
+// LFOs
+    'LFO_KEY_SYNC_LFO1',
+    'LFO_KEY_SYNC_LFO2',
+    'LFO_SPEED_SYNC_LFO1',
+    'LFO_SPEED_SYNC_LFO2',
+    'LFO_SLEW_LFO_1',
+    'LFO_SLEW_LFO_2',
+// Oscillator
+    'OSC_BEND_AMOUNT',
+    'OSC_1_2_SYNC',
+// Velocity
+    'VELOCITY_AMP_ENV',
+    'VELOCITY_MOD_ENV',
+// VCA
+    'VCA_LIMIT',
+// Arpeggiator
+    'ARP_SWING',
+    'ARP_SEQ_RETRIG',
+// Octave
+    'OCTAVE_KEY_TRANSPOSE',
+    'OCTAVE',
+// Misc
+    'OTHER_MOD',
+// Global
+    'GLOBAL_MIDI_CHAN',
+    'GLOBAL_LOCAL',
+    'GLOBAL_TUNE',
+    'GLOBAL_INPUT_GAIN'
+]);
--- src/bs2-sysex.js
+++ src/bs2-sysex.js
@@ -2,152 +2,152 @@
  * Bass Station II System Exclusive format
  */
 
-
-/*export const */
-
 BS2_SYSEX_LENGTH = 154;
-BS2_SYSEX = [
+BS2_SYSEX = [   // the number of masks define the number of bytes, so the "bytes" attribute is no longer necessary
     {
+        id: "SYSEX_START",
         offset: 0,
-        bytes: 1,
-        masks: [0xFF],
-        shift: 0,
+        //bytes: 1,
+        mask: [0xFF],
+        //shift: 0,
         range: [0xF0],
         description: "start of sysex data"
     }, {
+        id: BS2_PARAM.OSC1_RANGE,
         offset: 20,
-        bytes: 2,
-        masks: [0x07, 0x78],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x07, 0x78],
+        //shift: 0,
         range: [63, 66],
         description: "Osc 1 Range"
     },
     {
         offset: 21,
-        bytes: 2,
-        masks: [0x03, 0x7C],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x03, 0x7C],
+        //shift: 0,
         range: [-12.0, 12.0],
         description: "Osc 1 coarse"
     }, {
         offset: 22,
-        bytes: 2,
-        masks: [0x03, 0x7E],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x03, 0x7E],
+        //shift: 0,
         range: [-100, 100],
         description: "Osc 1 fine"
     }, {
         offset: 26,
-        bytes: 2,
-        masks: [0x1F, 0x20],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x1F, 0x20],
+        //shift: 0,
         range: [63, 66],
         description: "Osc 2 Range"
     }, {
         offset: 27,
-        bytes: 2,
-        masks: [0x1F, 0x70],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x1F, 0x70],
+        //shift: 0,
         range: [-12.0, 12.0],
         description: "Osc 2 coarse"
     },  {
         offset: 28,
-        bytes: 2,
-        masks: [0x0F, 0x78],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x0F, 0x78],
+        //shift: 0,
         range: [-100, 100],
         description: "Osc 2 fine"
     }, {
         offset: 98,
-        bytes: 2,
-        masks: [0x1F, 0x60],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x1F, 0x60],
+        //shift: 0,
         range: [-63, 63],
         description: "Osc 1 Mod Env depth"
     }, {
         offset: 37,
-        bytes: 1,
-        masks: [0x08],
-        shift: 0,
+        //bytes: 1,
+        mask: [0x08],
+        //shift: 0,
         range: [0, 0],
         description: "Sub Osc octave"
     }, {
         offset: 44,
-        bytes: 2,
-        masks: [0x0F, 0x78],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x0F, 0x78],
+        //shift: 0,
         range: [0, 255],
         description: "Filter Frequency"
     }, {
         offset: 90,
-        bytes: 2,
-        masks: [0x3F, 0x60],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x3F, 0x60],
+        //shift: 0,
         range: [-127, 127],
         description: "Osc 1 LFO 1 depth"
     }, {
         offset: 91,
-        bytes: 2,
-        masks: [0x1F, 0x70],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x1F, 0x70],
+        //shift: 0,
         range: [-127, 127],
         description: "Osc 2 LFO 1 depth"
     }, {
         offset: 99,
-        bytes: 2,
-        masks: [0x0F, 0x70],
-        shift: 0,
+        //bytes: 2,
+        mask: [0x0F, 0x70],
+        //shift: 0,
         range: [-63, 63],
         description: "Osc 2 Mod Env depth"
     }, {
     offset: 101,
-    bytes: 2,
-    masks: [0x03,0x7C],
-    shift: 0,
+    //bytes: 2,
+    mask: [0x03,0x7C],
+    //shift: 0,
     range: [-90, 90],
     description: "Osc 1 Pulse Width"
 },{
     offset: 102,
-    bytes: 2,
-    masks: [0x01,0x7E],
-    shift: 0,
+    //bytes: 2,
+    mask: [0x01,0x7E],
+    //shift: 0,
     range: [-90, 90],
     description: "Osc 2 Pulse Width"
 },{
     offset: 66,
-    bytes: 2,
-    masks: [0x3F,0x60],
-    shift: 0,
+    //bytes: 2,
+    mask: [0x3F,0x60],
+    //shift: 0,
     range: [0, 255],
     description: "LFO 1 Speed"
 },{
     offset: 73,
-    bytes: 2,
-    masks: [0x7F,0x40],
-    shift: 0,
+    //bytes: 2,
+    mask: [0x7F,0x40],
+    //shift: 0,
     range: [0, 255],
     description: "LFO 2 Speed"
 },{
     offset: 64,
-    bytes: 1,
-    masks: [0x7F],
-    shift: 0,
+    //bytes: 1,
+    mask: [0x7F],
+    //shift: 0,
     range: [0, 127],
     description: "LFO 1 Delay"
 },{
     offset: 70,
-    bytes: 2,
-    masks: [0x01,0x7E],
-    shift: 0,
+    //bytes: 2,
+    mask: [0x01,0x7E],
+    //shift: 0,
     range: [0, 127],
     description: "LFO 2 Delay"
 },{
         offset: 153,
-        bytes: 1,
-        masks: [0xFF],
-        shift: 0,
+        //bytes: 1,
+        mask: [0xFF],
+        //shift: 0,
         range: [0xF7],
         description: "end of sysex data"
     }
 ];
 
+console.log(BS2_SYSEX);
